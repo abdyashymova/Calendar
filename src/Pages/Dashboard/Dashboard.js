f31df4ac1c69d910e4646318ced4c3522688acc6
jamilya <abdyashymova>
2022-08-16 08:46:44 +0600

    feat:add main calendar for Dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Dashboard.module.css'
 import Header from "../../components/Header/Header";
+import {Calendar} from "antd";
 
 const Dashboard = () => {
     return (
         <div className={s.dashboard}>
         <Header/>
+        <Calendar headerRender={() =>false} className="calendar_table"/>
         </div>
     );
 };
